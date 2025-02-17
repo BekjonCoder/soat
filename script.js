0eb516c6adfd02cdfdc115e122959be11148d342
BekjonCoder <k75154256@gmail.com>
2025-02-17 18:12:39 +0500

    thg

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const date=new Date()
-console.log(date);
+const red=document.getElementById('red')
+const blue=document.getElementById('blue')
+const green=document.getElementById('green')
+const section=document.getElementsByTagName('section')
+red.addEventListener('click',()=>{
+    section.style.background='red'
+})
+blue.addEventListener('click',()=>{
+    section.style.background="blue"
+})
+green.addEventListener('click',()=>{
+    section.style.background='green'
+})
